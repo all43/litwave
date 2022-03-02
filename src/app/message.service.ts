9772e353b2d484779303e1394231dad378eafdc2
Evgenii Malikov <evgenii.malikov@taxmaro.com>
2022-03-02 20:45:48 +0100

    message service basic implementation

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
 import { MorseService } from './morse.service';
+import { from, Observable, of } from 'rxjs';
+import { concatMap, delay, distinctUntilChanged } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
+  public message = 'STOP WAR!';
+  public stream: Observable<boolean>;
+  private ditLength = 780; // dit length in milliseconds
 
   constructor(morse: MorseService) {
-    const morseEncoded = morse.encodeFormatted('STOP WAR');
-    const morseBinaryEncoded = morse.encodeBinary('STOP WAR');
-    console.log(morseEncoded);
-    console.log(morseBinaryEncoded);
+    const morseBinaryEncoded = morse.encodeBinary(this.message);
+    this.stream = from(morseBinaryEncoded).pipe(
+      concatMap((val) => of(val).pipe(
+        delay(this.ditLength),
+      )),
+      distinctUntilChanged(),
+    );
   }
 }
