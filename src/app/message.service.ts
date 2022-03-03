038bc748074c581521cdbc41b1ab1b06c1d6af50
Evgenii Malikov <evgenii.malikov@taxmaro.com>
2022-03-03 08:50:38 +0100

    turn off at cycle end

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,23 +1,27 @@
 import { Injectable } from '@angular/core';
 import { MorseService } from './morse.service';
 import { from, Observable, of } from 'rxjs';
-import { concatMap, delay, distinctUntilChanged } from 'rxjs/operators';
+import { concatMap, delay, distinctUntilChanged, endWith } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
-  public message = 'STOP WAR!';
+  public message = 'STOP WAR';
   public stream: Observable<boolean>;
-  private ditLength = 780; // dit length in milliseconds
+  public cycleLength: number;
+  private ditLength = 280; // dit length in milliseconds
 
   constructor(morse: MorseService) {
     const morseBinaryEncoded = morse.encodeBinary(this.message);
+    this.cycleLength = morseBinaryEncoded.length * this.ditLength;
+    console.log(this.cycleLength/1000);
     this.stream = from(morseBinaryEncoded).pipe(
       concatMap((val) => of(val).pipe(
         delay(this.ditLength),
       )),
       distinctUntilChanged(),
+      endWith(false), // switch off at the end
     );
   }
 }
