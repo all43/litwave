039ceeacb32ee2f426ad2b32e6b6d2931b4cd395
Evgenii Malikov <evgenii.malikov@taxmaro.com>
2022-03-12 08:05:13 +0100

    proper cancellation of running sequence

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,7 +1,7 @@
 import { App } from '@capacitor/app';
 import { Injectable } from '@angular/core';
 import { MorseService } from './morse.service';
-import { from, Observable, Subject, ReplaySubject, of, timer,  } from 'rxjs';
+import { from, Observable, ReplaySubject, of, timer, } from 'rxjs';
 import {
   concatMap,
   delay,
@@ -9,7 +9,6 @@ import {
   endWith,
   switchMap,
   share,
-  takeUntil,
 } from 'rxjs/operators';
 
 @Injectable({
@@ -18,13 +17,11 @@ import {
 export class MessageService {
   public message = 'STOP WAR'; // TODO: move to config
   public stream: Observable<boolean>;
-  public timeStream: Observable<number>;
   public morseStream: Observable<boolean>;
   public cycleLength: number;
   public repeatEvery: number;
   // replay subject used because regular subject could emit first value before subscriber connects
-  private trigger = new ReplaySubject<number>(1);
-  private stop = new Subject<void>();
+  private trigger = new ReplaySubject<number | null>(1);
   private ditLength = 300; // dit length in milliseconds
 
   constructor(morse: MorseService) {
@@ -33,9 +30,7 @@ export class MessageService {
     const pauseLength = this.ditLength * 14;
     // repeat every 30 or 60 seconds depenging on cycle length
     this.repeatEvery = this.cycleLength + pauseLength >= 30000 ? 60000 : 30000;
-    this.timeStream = this.trigger.pipe(
-      switchMap((initialDelay) => timer(initialDelay, this.repeatEvery)),
-    );
+    // take binary morse stream and emit values with timed delay
     this.morseStream = from(morseBinaryEncoded).pipe(
       concatMap((val) => of(val).pipe(
         delay(this.ditLength),
@@ -43,24 +38,31 @@ export class MessageService {
       distinctUntilChanged(),
       endWith(false), // switch off at the end
     );
-    this.stream = this.timeStream.pipe(
-      switchMap(() => this.morseStream),
-      takeUntil(this.stop),
+
+    this.stream = this.trigger.pipe(
+      switchMap((val) => val !== null ?
+        timer(val, this.repeatEvery).pipe(
+          switchMap(() => this.morseStream),
+        )
+        : of(false)),
       share(), // supposed to be shared by multiple subscribers
     );
     this.updateTimer();
     App.addListener('appStateChange', ({ isActive }) => {
       if (isActive) {
         this.updateTimer();
+      } else {
+        this.stopTimer();
       }
     });
   }
   updateTimer(): void {
-    // this.stop.next(); // cancel running timer
-    setTimeout(() => {
-      const timeout = this.timeToNextCycle();
-      this.trigger.next(timeout);
-    }, 0);
+    const timeout = this.timeToNextCycle();
+    this.trigger.next(timeout);
+  }
+
+  stopTimer() {
+    this.trigger.next(null);
   }
 
   timeToNextCycle(): number {
